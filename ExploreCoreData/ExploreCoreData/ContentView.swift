6b7324d28f7e44c6a9ec4dec2e36d02f780c0a59
sajid <sajidhasan673@gmail.com>
2023-03-09 23:57:50 +0600

    delete item with CoreData

--- ExploreCoreData/ExploreCoreData/ContentView.swift
+++ ExploreCoreData/ExploreCoreData/ContentView.swift
@@ -36,29 +36,31 @@ struct ContentView: View {
         withAnimation {
             let newFruit = FruitEntity(context: viewContext)
             newFruit.name = "new Apple"
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            
+            saveItems()
         }
     }
 
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
-            offsets.map { fruits[$0] }.forEach(viewContext.delete)
+//            offsets.map { fruits[$0] }.forEach(viewContext.delete)
+            guard let index = offsets.first else { return }
+            let fruitEntity = fruits[index]
+            viewContext.delete(fruitEntity)
             
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            
+            saveItems()
+        }
+    }
+    
+    private func saveItems() {
+        do {
+            try viewContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
     }
 }
